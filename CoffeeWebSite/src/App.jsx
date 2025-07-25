bd0b9d4170439080134adbbc60e7c7367934e56c
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-07-25 07:07:20 +0530

    Create App.jsx

---
+++ CoffeeWebSite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './Components/Navigation/Navigation'
+import Hero from './Components/Hero/Hero'
+import Stats from './Components/Stats/Stats'
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+      <Stats />
+    </>
+  )
+}
+
+export default App
