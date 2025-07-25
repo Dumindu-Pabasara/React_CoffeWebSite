d2a7def7035096298df81b809a4d3c319e59ed28
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-07-25 07:06:41 +0530

    Create Stats.jsx

---
+++ CoffeeWebSite/src/Components/Stats/Stats.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Stats.css'
+function Stats() {
+  return (
+    <>
+    {/* Stats Section */}
+    {/* Stats */}
+      <div className="stats">
+        <div className="stat-item">
+          <h3>50+</h3>
+          <p>Item Of Coffee</p>
+        </div>
+        <div className="stat-item">
+          <h3>20+</h3>
+          <p>Order Running</p>
+        </div>
+        <div className="stat-item">
+          <h3>2k+</h3>
+          <p>Happy Customer</p>
+        </div>
+      </div>
+      </>
+  )
+}
+
+export default Stats
