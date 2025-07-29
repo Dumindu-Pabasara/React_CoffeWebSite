d5dc9ec8cf0ab031642528cad55691143cdc6277
Dumindu_Ekanayake <137734956+Dumindu-Pabasara@users.noreply.github.com>
2025-07-29 20:53:27 +0530

    Update SecondScreen.jsx

--- CoffeeWebSite/src/Components/SecondScreen/SecondScreen.jsx
+++ CoffeeWebSite/src/Components/SecondScreen/SecondScreen.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import './SecondScreen.css'
+import heavenCoffeeImg from '../../assets/heavenCoffee.png'
 
 function SecondScreen() {
   return (
     <div className='secondpage'>
         <div className='heavenimage'>
-            <img src="heaven.jpg" alt="Coffee" />
+            <img src={heavenCoffeeImg} alt="Coffee" />
         </div>
         <div className='secondpagecontent'>
             <h1>Coffee Heaven</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorem, deleniti totam, aspernatur dolore sit 
-                modi impedit vitae consequuntur quae consectetur dolor labore tempora obcaecati aliquid ipsam 
-                voluptates unde quibusdam!</p>
+            <p>Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem Ipsum Has Been The Industry's Standard Dummy Text Ever Since The 1500s.</p>
             <button className='secondpagebutton'>View All</button>
 
         </div>
